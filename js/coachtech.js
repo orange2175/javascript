ac5253e68f4388cd7ab9643dbdb3258dedd5b1ff
orange2175 <105894413+orange2175@users.noreply.github.com>
2022-07-20 17:46:53 +0900

    Update coachtech.js

--- js/coachtech.js
+++ js/coachtech.js
@@ -1,6 +1,7 @@
-const target = document.getElementById("menu");
+const target = document.getElementById("hamburger");
 target.addEventListener('click', () => {
+  const target = document.getElementById("hamburger");
   target.classList.toggle('open');
-  const nav = document.getElementById("nav");
+  const nav = document.getElementById("globalMenuSp");
   nav.classList.toggle('in');
 });
